b073a548919195a7e84f445fed97a5b290ed12e6
mahmoudmohamed <mmaahm2002@gmail.com>
2024-04-11 22:13:05 +0200

    useClickOutside

--- app/hooks/useClickOutside.tsx
+++ app/hooks/useClickOutside.tsx
@@ -3,19 +3,19 @@ import { useCallback, useEffect, useRef } from 'react';
 function useClickOutside(close: () => void) {
     const ref = useRef<HTMLDivElement>(null);
 
-    const handleClickOutside = useCallback((event: MouseEvent) => {
+    const handleClickOutside = (event: MouseEvent) => {
         const element = ref.current;
         if (element && !element.contains(event.target as Node)) {
             close()
         }
-    }, [close]);
+    }
 
     useEffect(() => {
         document.addEventListener('mousedown', handleClickOutside);
         return () => {
             document.removeEventListener('mousedown', handleClickOutside);
         };
-    }, [handleClickOutside]);
+    }, []);
 
     return ref;
 }
